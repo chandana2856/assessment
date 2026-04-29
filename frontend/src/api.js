64046bee71f70a8417a0a68c947f9f6189602412
Daravath Chandana <daravathchandana56@gmail.com>
2026-04-29 23:03:58 +0530

    frontend deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://assessment-production-eb5a.up.railway.app/",
 });
 
 // 🔑 Generate idempotency key
